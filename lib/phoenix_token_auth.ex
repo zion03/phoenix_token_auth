abae7afcd0edea8e5b2aa4ec96d5af070913ade8
Dmitry Charnetskiy <zion03@mail.ru>
2017-01-07 23:08:22 +0300

    Update phoenix_token_auth.ex

--- lib/phoenix_token_auth.ex
+++ lib/phoenix_token_auth.ex
@@ -2,14 +2,14 @@ defmodule PhoenixTokenAuth do
 
   defmacro mount do
     quote do
-      post  "users",                 PhoenixTokenAuth.Controllers.Users, :create
-      post  "users/:id/confirm",     PhoenixTokenAuth.Controllers.Users, :confirm
-      post  "sessions",              PhoenixTokenAuth.Controllers.Sessions, :create
-      delete  "sessions",            PhoenixTokenAuth.Controllers.Sessions, :delete
-      post  "password_resets",       PhoenixTokenAuth.Controllers.PasswordResets, :create
-      post  "password_resets/reset", PhoenixTokenAuth.Controllers.PasswordResets, :reset
-      get   "account",               PhoenixTokenAuth.Controllers.Account, :show
-      put   "account",               PhoenixTokenAuth.Controllers.Account, :update
+      post  "/users",                 PhoenixTokenAuth.Controllers.Users, :create
+      post  "/users/:id/confirm",     PhoenixTokenAuth.Controllers.Users, :confirm
+      post  "/sessions",              PhoenixTokenAuth.Controllers.Sessions, :create
+      delete  "/sessions",            PhoenixTokenAuth.Controllers.Sessions, :delete
+      post  "/password_resets",       PhoenixTokenAuth.Controllers.PasswordResets, :create
+      post  "/password_resets/reset", PhoenixTokenAuth.Controllers.PasswordResets, :reset
+      get   "/account",               PhoenixTokenAuth.Controllers.Account, :show
+      put   "/account",               PhoenixTokenAuth.Controllers.Account, :update
     end
   end
 
