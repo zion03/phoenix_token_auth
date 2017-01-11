c904e7ec558167f46ce4e385e221d6919f469b47
Dmitry Charnetskiy <zion03@mail.ru>
2017-01-11 23:08:15 +0300

    Update sessions_controller.ex

--- lib/phoenix_token_auth/controllers/sessions_controller.ex
+++ lib/phoenix_token_auth/controllers/sessions_controller.ex
@@ -16,7 +16,7 @@ defmodule PhoenixTokenAuth.Controllers.Sessions do
   """
   def create(conn, %{"username" => username, "password" => password}) do
     case Authenticator.authenticate_by_username(username, password) do
-      {:ok, user} -> json conn, %{access_token: Authenticator.generate_token_for(user), token_type: "bearer", id: user.id}
+      {:ok, user} -> json conn, %{access_token: Authenticator.generate_token_for(user), token_type: "bearer", id: user.id, expires_in: PhoenixTokenAuth.Authenticator.token_validity_minutes*60}
       {:error, errors} -> Util.send_error(conn, errors, 401)
     end
   end
